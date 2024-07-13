730ec269716ddafbd06c8b3bcc020651c544d084
Blxtch <moreau.romain@hotmail.com>
2024-07-13 12:38:31 +0200

    ok

---
+++ packages/site/src/components/game/gameComponents/matrice.jsx
@@ -0,0 +1,15 @@
+import './matrice.css';
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Matrix of Cards</title>
+</head>
+<body>
+    <div id="container">
+        <div id="matrix"></div>
+        <div id="hand"></div>
+    </div>
+
+</body>
+</html>
