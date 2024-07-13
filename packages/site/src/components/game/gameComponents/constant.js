712e2c82b89028b6bc08086a24197dfc2b7effc5
Blxtch <moreau.romain@hotmail.com>
2024-07-13 17:28:03 +0200

    C BI N

--- packages/site/src/components/game/gameComponents/constant.js
+++ packages/site/src/components/game/gameComponents/constant.js
@@ -1,5 +1,14 @@
 
-  
+export const couleurs = [
+    '#FF5733',
+    '#33FF57',
+    '#3357FF',
+    '#F333FF',
+    '#FF33A6',
+    '#FF8F33',
+    '#33FFF3',
+    '#8F33FF',
+  ];
   export const playerColors = {
     player1: '#FFFFFF', // Gold for Player 1
     player2: '#111111', // GreenYellow for Player 2
