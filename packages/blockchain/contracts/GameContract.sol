e77f9a18f61557fb9c3ea325b825078edaf9d950
Maxime Delhaye <62051111+TendosBG@users.noreply.github.com>
2024-07-14 08:12:10 +0200

    Update GameContract.sol

--- packages/blockchain/contracts/GameContract.sol
+++ packages/blockchain/contracts/GameContract.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+// This contract was successfully deployed on Linea Sepolia.
+
 contract GameContract {
     address public player1;
     address public player2;
